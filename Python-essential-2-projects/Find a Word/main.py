d4c40f3c0fb189fd3c23278b525ac99b910b4305
mohamedezz96 <100701600+mohamedezz96@users.noreply.github.com>
2026-01-08 13:13:33 +0200

    Update main.py

--- Python-essential-2-projects/Find a Word/main.py
+++ Python-essential-2-projects/Find a Word/main.py
@@ -1,13 +1,2 @@
-user_input_1 = input("Enter Your Word: ")
-user_input_2 = list(input("Enter Your sequence of letters: "))
-length_check = len(user_input_1)
 
-for char in user_input_1:
-    if char in user_input_2:
-        length_check -= 1
-
-if length_check == 0:
-    print("Yes")
-else:
-    print("No")
   
