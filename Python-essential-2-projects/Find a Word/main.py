17cbeeb75fc87ddfae8c420ddee9d43ee4760580
mohamedezz96 <100701600+mohamedezz96@users.noreply.github.com>
2026-01-09 17:31:26 +0200

    Update main.py

--- Python-essential-2-projects/Find a Word/main.py
+++ Python-essential-2-projects/Find a Word/main.py
@@ -1,2 +1,16 @@
+user_input_1 = input("Enter Your Word: ")
+user_input_2 = input("Enter Your sequence of letters: ")
+final_result = True
+result = 0 
+for char in user_input_1:
+    result = user_input_2.find(char, result)
+    if result == -1:
+        final_result = False
+        break
+    else:
+        result += 1
 
-  
+if final_result:
+    print("Yes")
+else:
+    print("No")
