7b8a64e6440ec2ca2743749ce30f52a43c352ca4
mohamedezz96 <100701600+mohamedezz96@users.noreply.github.com>
2026-01-13 17:26:14 +0200

    Create main.py

---
+++ Python-essential-2-projects/Queue aka FIFO/main.py
@@ -0,0 +1,31 @@
+class QueueError(IndexError):  # Choose base class for the new exception.
+    pass
+
+
+
+class Queue:
+    def __init__(self):
+        self.__queue = []
+
+
+    def put(self, elem):
+        self.__queue.insert(0, elem)
+
+
+    def get(self):
+        val = self.__queue[-1]
+        del self.__queue[-1]
+        return val
+
+
+
+que = Queue()
+que.put(1)
+que.put("dog")
+que.put(False)
+try:
+    for i in range(4):
+        print(que.get())
+except:
+    print("Queue error")
+    
